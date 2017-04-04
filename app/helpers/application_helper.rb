5bfba507642313578ed6e61eda38838e417d9b6c
Amz K <akintner@u.rochester.edu>
2017-04-04 16:13:24 -0600

    change helper method to add login page styling for users/new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
    def page_is_login_or_create?
-    current_uri(request) == "/login" 
+    current_uri(request) == "/login" || current_uri(request) == "/users/new" 
   end
 
   def current_uri(request)
